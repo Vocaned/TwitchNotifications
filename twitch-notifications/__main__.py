5498fb3c7975bbec0ea39d339a1c770abb9af9b9
Vocaned <10358674+Vocaned@users.noreply.github.com>
2020-10-02 13:46:34 +0300

    kpö3er

--- twitch-notifications/__main__.py
+++ twitch-notifications/__main__.py
@@ -1,18 +1,39 @@
+import os
+import json
+import re
+import config
+from notification import sendNotif
 from sock import Socket
-socket = Socket(('irc.twitch.tv', 6667), password='asd', nick='Fam0r', join='fam0r')
 
 if __name__ == "__main__":
+    c = config.getConfig()
+
+    socket = Socket(('irc.twitch.tv', 6667), password=c['password'], nick=c['username'], join=c['channels'])
     while True:
         data = socket.readPacket()
         if not data: continue
 
         for message in data.split('\r\n'):
-            msgtype = message.split(' ')[0]
-            
-            if msgtype == 'PING':
-                socket.sendPacket(message.replace('PING', 'PONG'))
-            elif msgtype == 'PRIVMSG':
-                username = data.split('!')[0].lstrip(':')
-                message = ' '.join(message.split()[4:])[1:]
+            if not message:
+                continue
+            print('< ' + message)
+            msg = message.split(' ')
 
-                print(username, message)
+            if msg[0] == 'PING':
+                socket.sendPacket('PONG ' + msg[1])
+            elif msg[1] == 'PRIVMSG':
+                channel = data.split('#')[1].split(' :')[0]
+                username = data.split('!')[0].lstrip(':')
+                message = ':'.join(message.split(':')[2:])
+
+                if message.startswith('\x01'):
+                    message = message.replace('\x01ACTION', '/me').replace('\x01', '')
+
+                if channel in c['channelNotifs']:
+                    sendNotif(channel, username, message)
+                    continue
+
+                for regex in c['regexNotifs']:
+                    if re.search(regex, message):
+                        sendNotif(channel, username, message)
+                        continue
